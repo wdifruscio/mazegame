c62a734c9e8e2c142b07167da0fed510532b41c6
willcodes <difruscio.dev@gmail.com>
2016-10-01 22:43:15 -0400

    first upload

---
+++ js/timer.js
@@ -0,0 +1,14 @@
+var count=15;
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+function timer()
+{
+  count=count-1;
+  if (count < 0)
+  {
+     timerUp = true;
+     endGame();
+     clearInterval(counter);
+     return;
+  }
+ document.getElementById("time").innerHTML=count + "s"; // watch for spelling
+}
